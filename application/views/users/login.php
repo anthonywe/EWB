5c40a9e8fbb4a6c65fc455bdce9e7bad7e363a2e
reshma1284 <reshma1284@gmail.com>
2017-11-12 12:27:05 +0100

    added login view

---
+++ application/views/users/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML> 
+<html>
+	<head>
+		<meta charset = "utf-8"/>
+		<title>Flower World</title>
+		<meta name="description" content="Information about different flowers in Europe.">		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+
+	<body>
+		
+		<h1> Welcome</h1>
+	</body>
+	</html>
