cf0dd05a5ff053dfb397e95e03e70bd7fe28414c
reshma1284 <reshma1284@gmail.com>
2017-11-14 14:58:33 +0100

    changing base url

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://localhost/test/MVC/EWB/';
+$config['base_url'] = 'http://localhost/Something/MVC/EWB/';
 
 /*
 |--------------------------------------------------------------------------
