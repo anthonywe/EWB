ca4abd855350931ac489f8551281639381e132f3
reshma1284 <reshma1284@gmail.com>
2017-11-15 18:36:32 +0100

    conf

--- application/models/User.php
+++ application/models/User.php
@@ -41,25 +41,5 @@ class User extends CI_Model
             )->row_array();        
     }
 
-
-    // public function addNGOtoLogin( $item )
-    // {
-    //     $query = "INSERT INTO ngo (name, contact_person, email, fields_of_activities, website, profile_pic) VALUES ( ?, ?, ?, ?, ?, ?)";
-    //     $values = array(
-    //         $item['ctl_name'],
-    //         $item['ctl_cperson'],
-    //         $item['ctl_email'],
-    //         $item['ctl_activities'],
-    //         $item['ctl_website'],
-    //         $item['ctl_pic']
-    //     );
-    //     $this->db->query($query, $values);
-    // }
-
-    
-    // public function listOfAll()
-    // {
-    //     return $this->db->query("SELECT * FROM users")->result_array();     
-    // }
 }
 
