5ccfbcbd04efa8e4d889a6f3ddff41712f38ac43
reshma1284 <reshma1284@gmail.com>
2017-11-15 19:30:59 +0100

    changed the base url in forum-main

--- application/views/users/forum-main.php
+++ application/views/users/forum-main.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-	<link rel="stylesheet" href="http://localhost/Something/MVC/EWB/public/main.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>public/main.css">
 </head>
 <body>
 	<nav class="navbar sticky-top nave-barC">
